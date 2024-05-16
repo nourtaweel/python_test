e63750aaed3dfb9dddd087ff57cf66b275ea9456
nourtaweel <nour.taweel@gmail.com>
2024-05-16 15:31:21 +0100

    pair programming test

---
+++ add.py
@@ -0,0 +1,5 @@
+# Write a Python program that takes two numbers from the user, add them, then print the result.
+x = input("input first number:")
+y = input("input second number:")
+z = int(x) + int(y)
+print(f"The result for {x} + {y} = {z}")
